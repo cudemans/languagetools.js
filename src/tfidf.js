5e0071cb336be9ef5665bfff01ab6478b2e660c7
Chris Udemans <chris.udemans@gmail.com>
2023-03-01 18:13:55 +0200

    build: update tfidf function

--- src/tfidf.js
+++ src/tfidf.js
@@ -3,15 +3,15 @@ import simpleTokeniser from "../utils/simpleTokeniser.js";
 export default function tfidf(docs) {
   const processed = simpleTokeniser(docs);
   // Term frequency
-  let tracker = [];
-  processed.forEach((d) => {
-    tracker.push(
-      d.reduce((acc, item) => {
-        acc[item] = ((acc[item] || 0) + 1) / d.length;
-        return acc;
-      }, {})
-    );
-  });
+  // let tracker = [];
+  // processed.forEach((d) => {
+  //   tracker.push(
+  //     d.reduce((acc, item) => {
+  //       acc[item] = ((acc[item] || 0) + 1) / d.length;
+  //       return acc;
+  //     }, {})
+  //   );
+  // });
 
   const totalWords = [...new Set(processed.flatMap((d) => d))];
 
@@ -25,4 +25,31 @@ export default function tfidf(docs) {
       })
     ),
   ];
+
+  const tfidf = processed.map((a) => {
+    const obj = {};
+    const wordLength = a.length;
+
+    totalWords.forEach((word) => {
+      const numDocs = documentFrequency.find((d) => d.word == word).freq;
+
+      // TF
+      // const tf = Object.values(
+      //   a.reduce((acc, item) => {
+      //     acc[item] = ((acc[item] || 0) + 1) / wordLength;
+      //     return acc;
+      //   }, {})
+      // );
+
+      const tf = a.filter((d) => d == word).length / wordLength;
+
+      //IDF
+      const idf = Math.log(processed.length / numDocs);
+
+      obj[word] = tf * idf;
+    });
+    return obj;
+  });
+
+  console.log(tfidf);
 }
